a6c326c3fe74c58737615f65ec7d1f264bb35e3e
Дмитрий Соловьёв <d1masolo19@mail.ru>
2023-08-24 16:35:09 +0300

    123

---
+++ MathGame/Models/GetLevels.cs
@@ -0,0 +1,31 @@
+﻿using MathGame.ViewModel;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MathGame.Models
+{
+    public static class Levels
+    {
+        public static ObservableCollection<LevelVeiwModel> GetLevels()
+        {
+            var levels = new ObservableCollection<LevelVeiwModel>();
+
+            for (var i = 0; i < 10; i++) 
+            {
+                levels.Add(new LevelVeiwModel()
+                {
+                    Riddle = i.ToString() + "=?",
+                    Answer = i.ToString(),
+                    RiddleVisual = new TextBlock()
+                    {
+                        Text = i.ToString(),
+                        Foreground = Brushes.Gray,
+                    },
+                });
+            }
+            return levels;
+        }
+
+    }
+}
--- MathGame/ViewModel/MainWindowViewModel.cs
+++ MathGame/ViewModel/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -9,27 +10,30 @@ namespace MathGame.ViewModel
 {
     public class MainWindowViewModel : BaseViewModel
     {
+
+        public Page CurrentPage { get; set; }
+        public LevelVeiwModel SelectLevel { get; set; }
+        public LevelVeiwModel CurrentLevel { get; set; }
+        public string InAnswer { get; set; } = "";
+        public ObservableCollection<LevelVeiwModel> Levels { get; set; } = new ObservableCollection<LevelVeiwModel>();
+
         private readonly Page Main;
         private readonly Page LevelSelection;
-        public readonly Page LevelPlay;
-        public Page CurrentPage { get; set; }
-
-        public LevelProvaderViewModel LevelProvader { get; set; }
-
-        public ObservableCollection<LevelVeiwModel> Levels { get; set; } = new ObservableCollection<LevelVeiwModel>();
+        private readonly Page LevelPlay;
+        private int MaxLevelProd { get; set; }
 
         public MainWindowViewModel()
         {
             Main = new Pages.Main(this);
             LevelSelection = new Pages.LevelSelection(this);
             LevelPlay = new Pages.Level(this);
-
-            LevelProvader = new LevelProvaderViewModel(this);
-
             CurrentPage = Main;
+            Levels = MathGame.Models.Levels.GetLevels();
+            CurrentLevel = Levels.First();
+
         }
 
-
+        #region Command
         public ICommand cmdLevelSelection
         {
             get
@@ -50,10 +54,59 @@ namespace MathGame.ViewModel
         {
             get
             {
- 
-                return new RelayCommand(() => CurrentPage = LevelPlay);
+                return new RelayCommand(() =>
+                {
+                    CurrentPage = LevelPlay;
+                    CurrentLevel = Levels.First();
+                });
             }
         }
 
+
+        public ICommand cmdNextLevel
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    if (InAnswer == CurrentLevel.Answer)
+                    {
+                        CurrentLevel.IsPassed = true;
+                        int l = Levels.IndexOf(CurrentLevel);
+                        CurrentLevel = Levels[l + 1];
+                        InAnswer = "";
+                        MaxLevelProd++;
+                    }
+
+                });
+            }
+        }
+        public ICommand cmdGoLevel
+        {
+            get
+            {
+                return new RelayCommand<LevelVeiwModel>((level) =>
+                {
+                    if (level.IsPassed)
+                    {
+                        CurrentPage = LevelPlay;
+                        CurrentLevel = level;
+                    }
+                });
+            }
+        }
+
+        public ICommand cmdGoEndLevel
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    CurrentPage = LevelPlay;
+                    CurrentLevel = Levels[MaxLevelProd];
+                });
+            }
+        }
+        #endregion
     }
 }
